85418703be043bbdecf8e85b080eb67ae8355632
Jeffrey Yang <yjeffrey@seas.upenn.edu>
2023-12-11 16:15:21 -0500

    add minimax prop test

--- test/GameAITest.hs
+++ test/GameAITest.hs
@@ -13,6 +13,13 @@ instance Arbitrary SmallInt where
     n <- choose (1, 2)
     return $ SmallInt n
 
+-- minimax finds the best move for the player at depth 1
+prop_minimaxDepthOne :: C.GameState -> Property
+prop_minimaxDepthOne g =
+  not (gameOver g) ==> snd (minimaxSearch g 1) == maximum (map (`evaluate` p) (update g <$> generateMoves g))
+  where
+    p = player g
+
 prop_negamaxSearch :: SmallInt -> C.GameState -> Property
 prop_negamaxSearch (SmallInt d) g =
   not (gameOver g) ==> snd (minimaxSearch g d) == snd (negamaxSearch g d)
