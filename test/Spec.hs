a9c4cc5b018c0789b4ffb9ba31eddf9f4429978c
Jeffrey Yang <yjeffrey@seas.upenn.edu>
2023-12-12 02:50:25 -0500

    uncomment tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,15 +7,15 @@ import Test.QuickCheck
 
 main :: IO ()
 main = do
-  -- putStrLn "*** Testing Parser ***"
-  -- CPT.test_all
-  -- CPT.qc
-  -- putStrLn "*** Testing Move Validity ***"
-  -- CT.test_all
-  -- CT.qc
-  -- putStrLn "*** Testing Simple Evaluation ***"
-  -- CSAT.test_all
-  -- CSAT.qc
+  putStrLn "*** Testing Parser ***"
+  CPT.test_all
+  CPT.qc
+  putStrLn "*** Testing Move Validity ***"
+  CT.test_all
+  CT.qc
+  putStrLn "*** Testing Simple Evaluation ***"
+  CSAT.test_all
+  CSAT.qc
   putStrLn "*** Testing Game AI ***"
   GAT.qc
   return ()
